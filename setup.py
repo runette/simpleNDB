456c8f70d3374ea3c5eeb8ab414ec831d720e9c3
runette <runette@gmail.com>
2019-04-17 14:27:04 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpleNDB",
+    version= "1.0.0",
+    author="Paul Harwood",
+    author_email="runette@gmail.com",
+    description="A very simple package to make Google Cloud Clients look more like appengine.api and NDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/runette/simpleNDB",
+    install_requires=[
+        "grpcio",
+        "google-cloud-datastore",
+        "google-cloud-storage",
+        "Pillow",
+        "attrdict"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
